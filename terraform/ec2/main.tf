97406280a434aa8958e5f56bf4194e3059575b6e
Akhil Rasheed <akhilrasheed16@gmail.com>
2024-09-28 16:01:00 +0530

    Expose port 8000 on the backend ec2

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -78,6 +78,12 @@ resource "aws_security_group" "thm_security_group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
